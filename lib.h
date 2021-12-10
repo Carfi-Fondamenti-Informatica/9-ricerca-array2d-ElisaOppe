1cecc47c63e1610e5d94734b7559bdfe700c1558
ElisaOppe <91607662+ElisaOppe@users.noreply.github.com>
2021-12-10 22:34:05 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,6 @@
+#ifndef ESERCITAZIONE9DEL12_12_2021_LIB_H
+#define ESERCITAZIONE9DEL12_12_2021_LIB_H
 
+bool ricerca(char lista [10][20], char nome[1][20], int &pos);
+
+#endif //ESERCITAZIONE9DEL12_12_2021_LIB_H
